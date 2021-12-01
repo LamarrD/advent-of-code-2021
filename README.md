f8d25b37343867109d05dfc5f78a09caf9ac6d02
lamarrd <lamarr@stedi.com>
2021-12-01 02:27:01 -0500

    feat: day1

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# advent-of-code-2021
-Advent of code challenges for 2021
+# Advent of Code 2021
+
+Welcome to my attempt to actually complete advent of code this year!
+
+"What's [Advent of Code](https://adventofcode.com/2021/about)"?
+TLDR; Small puzzles that release every day for the month of December up until Christmas.
+Lots of people use Advent of Code to try out new languages, IDEs or compete with their friends.
+
+"What's your goal for AOC 2021"
+Well i'm glad ya asked. I'd like to accomplish a few things:
+
+- ...finish for once
+- be organized (actually commit my code to GitHub each day)
+- write _clean_ solutions - I want to write code that I could explain/teach people easily.
+  It can be more verbose and/or less efficient if it's easier to read and understand.
+  If I'm feeling frisky some days we can do an easy to read version + some cooler 1-liners
+- @ballpointcarrot has given me the courage to maybe live stream some of these. TBD
+
+Let's get it.
---
+++ day1.py
@@ -0,0 +1,45 @@
+# We need to calculate the # of times the depth increases
+def part1():
+    depth_increases = 0
+    depths = [int(line.strip()) for line in open('day1.input')]
+    prev_depth = depths[0]
+    for depth in depths:
+        if prev_depth < depth:
+            depth_increases += 1
+        prev_depth = depth
+    print(f"There were {depth_increases} depth increases")
+
+# Now we need to count the number of times the depth increases over a window of 3 depths.
+def part2():
+    depth_increases = 0
+    depths = [int(line.strip()) for line in open('day1.input')]
+    for i in range(len(depths)-3):
+        a1 = depths[i]
+        b1 = depths[i+1]
+        c1 = depths[i+2]
+        window1 = a1 + b1 + c1
+
+        a2 = depths[i+1]
+        b2 = depths[i+2]
+        c2 = depths[i+3]
+        window2 = a2 + b2 + c2
+
+        if window1 < window2:
+            depth_increases += 1
+    print(f"There were {depth_increases} depth increases across a 3 value window")
+
+# 1-linerish versions 
+def part1b():
+    depths = [int(line.strip()) for line in open('day1.input')]
+    depth_increases = len([1 for i in range(len(depths))[1:] if depths[i-1] < depths[i]])
+    print(f"There were {depth_increases} depth increases")
+
+def part2b():
+    depths = [int(line.strip()) for line in open('day1.input')]
+    depth_increases = len([i for i in range(len(depths)-3) if sum(depths[i:i+3]) < sum(depths[i+1:i+4])])
+    print(f"There were {depth_increases} depth increases across a 3 value window")
+
+if __name__ == "__main__":
+    part1()
+    part1b()
+    part2()
